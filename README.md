f514056d6a7e3d9005b925438116d93ea5b510f0
ouokki <ouokki.7zgui@gmail.com>
2022-09-15 17:01:54 +0100

    Partition implementation
    Tests for implementation succeded

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## This is the Partition library
 ### I am following the TDD (Test driven development) approach
 #### Writing tests using Junit4
+#### Write a "partition" function that takes a "list" parameter and a "size" parameter and returns a list of sublists, where each sublist has at most "size" elements.
--- src/com/partitionImplementation/Partition.java
+++ src/com/partitionImplementation/Partition.java
@@ -1,6 +1,7 @@
 package com.partitionImplementation;
 
 import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,17 +18,36 @@ public class Partition<U> extends AbstractList<List<U>> {
         this.size = size;
     }
 
+    /**
+     * The function partition takes as parameters a list and the size of subElements divide on
+     * @param list : list of elements
+     * @param size : the size of the sub lists to divide on
+     * @return returns a list of sub lists and the size
+     */
     public static <U> Partition<U> partition(List<U> list, int size) {
         return new Partition<>(list, size);
     }
 
+    /**
+     * The function partition takes index as parameter
+     * @param index : list of elements
+     * @return returns a list of sub lists
+     */
     @Override
     public List<U> get(int index) {
-        return null;
+        int start = index * size;
+        int end = Math.min(start + size, list.size());
+        return new ArrayList<>(list.subList(start, end));
     }
 
+    /**
+     * @return returns the number of sub lists
+     */
     @Override
     public int size() {
-        return 0;
+        return (int) Math.ceil((double) list.size() / (double) size);
     }
+
+
+
 }
