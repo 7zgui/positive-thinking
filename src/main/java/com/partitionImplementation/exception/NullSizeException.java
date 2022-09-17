8dd62518d993041de9ca10a2f07ba037bcd61e6f
ouokki <ouokki.7zgui@gmail.com>
2022-09-17 18:43:48 +0100

    Correct NullSizeException

--- src/main/java/com/partitionImplementation/exception/NullSizeException.java
+++ src/main/java/com/partitionImplementation/exception/NullSizeException.java
@@ -4,7 +4,7 @@ package com.partitionImplementation.exception;
  * @author Mohamed ouokki on 9/15/22
  * @project positive-thinking
  */
-public class NullSizeException extends IllegalArgumentException{
+public class NullSizeException extends OutOfMemoryError{
     private static final long serialVersionUID = 4664456874499611218L;
 
     private String errorCode="Size of sub lists should be greater than 0";
